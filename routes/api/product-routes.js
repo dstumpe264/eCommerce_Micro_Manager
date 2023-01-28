e752c11b379491cb9e6131af643640ae4c46cd94
David Stumpe <dstumpe264@gmail.com>
2023-01-28 17:00:27 -0500

    updated product routes, tested with insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,19 +3,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  // find all categories
+  try {
+    const productsData = await Product.findAll({
+      include: [{model: Category},{model: Tag}],
+    });
+    res.status(200).json(productsData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  // be sure to include its associated categories and tags
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async(req, res) => {
+  // find one product by its `id` value
+  // be sure to include its associated category and tag
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(400).json({message: 'No product found with that id'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-// create new product
+// create a new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
